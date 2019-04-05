097771c011f60db3d776955f0b75c4941b1df33d
CoenSteuns <c.steuns@gmail.com>
2019-04-05 13:18:36 +0200

    Game unlock & temp marker

---
+++ Assets/Scripts/GameUnlock/ImageScanner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+public class ImageScanner : MonoBehaviour
+{
+
+    [SerializeField]
+    private ImageTargetBehaviour imageTarget;
+
+    public UnityEvent OnImageTracked;
+
+    private void Awake() {
+        StartCoroutine(WaitForImageTracked());
+    }
+
+    private IEnumerator WaitForImageTracked(){
+        while(imageTarget.CurrentStatus != TrackableBehaviour.Status.TRACKED){
+            yield return null;
+        }
+        
+        OnImageTracked.Invoke();
+    }
+
+
+}
