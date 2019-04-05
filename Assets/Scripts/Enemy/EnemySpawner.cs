6c6f1e574c24ca5b83a62018e2343a7d4393de61
jvdwijk <joeyvdwijk@gmail.com>
2019-04-05 14:09:14 +0200

    EnemySpawner Started

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Peppasquad.Enemy
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        public void SpawnEnemy(){
+            
+        }
+
+        public void SpawnBoss(){
+            
+        }
+
+        private void SpawnEntity(){
+
+        }
+    }
+}
