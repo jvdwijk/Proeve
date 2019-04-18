53aa2df32a7ba2fa4f0de69d62249e976232bd63
CoenSteuns <c.steuns@gmail.com>
2019-04-18 13:19:16 +0200

    Stats system

---
+++ Assets/Scripts/PlayerStats/Stat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat<T>
+where T : Enum
+{
+    private float value;
+    private T statType;
+
+    public event Action<Stat<T>> StatChanged;
+
+    public T StatType => statType;
+    public float Value{
+        get { return this.value; }
+        set { SetValue(value); }
+    }
+
+    public Stat(T statType)
+    {
+        this.statType = statType;
+    }
+
+    public void SetValue(float value){
+        this.value = value;
+        StatChanged.Invoke(this);
+    }
+
+}
---
+++ Assets/Scripts/PlayerStats/StatsHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsHandler<T> : MonoBehaviour 
+where T : System.Enum 
+{
+    private Dictionary<T, Stat<T>> stats;
+
+    public event Action<Stat<T>> StatCreated;
+
+    public Stat<T> GetStat(T statType){
+        if (!stats.ContainsKey(statType))
+            CreateStat(statType);
+
+        return stats[statType];
+    }
+
+    public void SetStatValue(T statType, float statValue){
+        var stat = GetStat(statType);
+        stat.Value = statValue;
+    }
+
+    private void CreateStat(T statType){
+        if (stats.ContainsKey(statType))
+            return;
+
+        var newStat = new Stat<T>(statType);
+        stats.Add(statType, newStat);
+        StatCreated?.Invoke(newStat);
+    }
+}
