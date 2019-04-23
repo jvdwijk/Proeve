698956eca229eec3b555c037b8f737dab0ced82c
jvdwijk <joeyvdwijk@gmail.com>
2019-04-23 12:03:06 +0200

    Enemy Spawner Finished

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,21 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-namespace Peppasquad.Enemy
+using PeppaSquad.UI;
+
+namespace PeppaSquad.Enemy
 {
     public class EnemySpawner : MonoBehaviour
     {
+        private Enemy[] enemyPrefabs;
+
+        private Enemy[] bossPrefabs;
+
+        private float spawnRadius = 5;
+
+        private HealthGUI healthGUI;
+
+        public event Action<Enemy> OnEnemySpawned;
+
         public void SpawnEnemy(){
-            
+            Enemy newEnemy = SpawnEntity(enemyPrefabs);
+            InitEntity(newEnemy);
         }
 
         public void SpawnBoss(){
-            
+            Enemy newEnemy = SpawnEntity(bossPrefabs);
+            InitEntity(newEnemy);
         }
 
-        private void SpawnEntity(){
+        private Enemy SpawnEntity(Enemy[] prefabs){
+            var enemyNumber = UnityEngine.Random.Range(0, prefabs.Length - 1);
+            return Instantiate(prefabs[enemyNumber]);
+        }
 
+        private void InitEntity(Enemy enemy){
+            
+            enemy.OnHealthChanged += healthGUI.ChangeHealth;
+            enemy.StartAttack();
+            OnEnemySpawned?.Invoke(enemy);
         }
     }
 }
