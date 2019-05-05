3fc31770c90f25b4dbabf5ede5e607726202d7e8
Justin <justinvg3@gmail.com>
2019-05-05 17:31:23 +0200

    GUI, Music/SFX/Mute, Audio Mixer Groups

---
+++ Assets/Scripts/VolumeHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeHandler : MonoBehaviour
+{
+public AudioMixer MusicMixer;
+public Slider MusicSlider;
+public Slider SfxSlider;
+public Toggle MuteToggle;
+
+public void SetLevelMusic(){
+    
+    MusicMixer.SetFloat("MusicVol", Mathf.Log10(MusicSlider.value)*20);
+}
+public void SetLevelSFX(){
+    
+    MusicMixer.SetFloat("SfxVol", Mathf.Log10(SfxSlider.value)*20);
+}
+public void ToggleMute(){
+    print("Peppa");
+    if(!MuteToggle.isOn)
+    {
+    MusicMixer.SetFloat("MasterVol",  Mathf.Log10(0.0001f)*20);
+    }
+    else
+    {
+    print("ReversePeppa");
+    MusicMixer.ClearFloat("MasterVol");
+    }
+}
+
+}
