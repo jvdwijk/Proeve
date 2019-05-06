7ce69588df8a967e2d743816921a8180698903f8
jvdwijk <joeyvdwijk@gmail.com>
2019-05-06 18:47:55 +0200

    Game Manager start + IResettable

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using PeppaSquad.Utils;
+using PeppaSquad.Enemies;
+
+namespace PeppaSquad.GameFlow
+{
+    public class GameManager : MonoBehaviour
+    {
+
+    }
+}
