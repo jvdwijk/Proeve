0ef219b89dcbaf729230b275978c29b8d3e3b76a
CoenSteuns <c.steuns@gmail.com>
2019-05-07 14:45:23 +0200

    Added levelopener to GameFlow namespace

--- Assets/Scripts/ARLevelHandling/LevelOpener.cs
+++ Assets/Scripts/ARLevelHandling/LevelOpener.cs
@@ -5,40 +5,42 @@ using UnityEngine;
 using Vuforia;
 using Vuforia.UnityCompiled;
 
-public class LevelOpener : MonoBehaviour, ITrackableEventHandler {
+namespace PeppaSquad.GameFlow {
+    public class LevelOpener : MonoBehaviour, ITrackableEventHandler {
 
-    private bool hasAlreadyTracked = false;
+        private bool hasAlreadyTracked = false;
 
-    [SerializeField]
-    private TrackableBehaviour trackableBehavior;
+        [SerializeField]
+        private TrackableBehaviour trackableBehavior;
 
-    [SerializeField]
-    private GameObject level;
+        [SerializeField]
+        private GameObject level;
 
-    private void Awake() {
-        trackableBehavior?.RegisterTrackableEventHandler(this);
-    }
-
-    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
-
-        transform.localScale = Vector3.one;
-        if (TrackedFirstTime(previousStatus, newStatus)) {
-            hasAlreadyTracked = true;
-            level.SetActive(true);
+        private void Awake() {
+            trackableBehavior?.RegisterTrackableEventHandler(this);
         }
 
-        if (LostTracking(previousStatus, newStatus)) {
-            transform.localScale = Vector3.zero;
+        public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+
+            transform.localScale = Vector3.one;
+            if (TrackedFirstTime(previousStatus, newStatus)) {
+                hasAlreadyTracked = true;
+                level.SetActive(true);
+            }
+
+            if (LostTracking(previousStatus, newStatus)) {
+                transform.localScale = Vector3.zero;
+            }
         }
-    }
 
-    private bool LostTracking(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
-        return newStatus == TrackableBehaviour.Status.NO_POSE && previousStatus == TrackableBehaviour.Status.EXTENDED_TRACKED ||
-            newStatus == TrackableBehaviour.Status.NO_POSE && previousStatus == TrackableBehaviour.Status.TRACKED;
-    }
+        private bool LostTracking(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+            return newStatus == TrackableBehaviour.Status.NO_POSE && previousStatus == TrackableBehaviour.Status.EXTENDED_TRACKED ||
+                newStatus == TrackableBehaviour.Status.NO_POSE && previousStatus == TrackableBehaviour.Status.TRACKED;
+        }
 
-    private bool TrackedFirstTime(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
-        return newStatus == TrackableBehaviour.Status.TRACKED && !hasAlreadyTracked;
-    }
+        private bool TrackedFirstTime(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+            return newStatus == TrackableBehaviour.Status.TRACKED && !hasAlreadyTracked;
+        }
 
+    }
 }
