3e795ba87762a09bd28fa0ad16db8fa9d6b71902
CoenSteuns <c.steuns@gmail.com>
2019-05-21 15:10:58 +0200

    Temp scripts

---
+++ Assets/Scripts/PickUp/Boost Stats/BoostType.cs
@@ -0,0 +1,5 @@
+﻿public enum BoostType {
+    Time,
+    Damage,
+    Credits,
+}
---
+++ Assets/Scripts/PickUp/Pick Ups/BoostEffects/StatEnhancingBoost.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatEnhancingBoost : BoostEffect {
+    [SerializeField]
+    private BoostStatHandler stats;
+
+    [SerializeField]
+    private BoostType statType;
+
+    public override void Boost() {
+        var stat = stats.GetOrCreateStat(statType);
+        stat.Value++;
+    }
+}
