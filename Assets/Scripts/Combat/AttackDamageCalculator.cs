57a631141f8752f1a65ba1d597efb0a302c56d83
CoenSteuns <c.steuns@gmail.com>
2019-05-23 12:30:36 +0200

    Boost effect in game

---
+++ Assets/Scripts/Combat/AttackDamageCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PeppaSquad.Pickups.Stats;
+using PeppaSquad.Stats.PlayerStats;
+
+public class AttackDamageCalculator : MonoBehaviour {
+
+    [SerializeField]
+    private PlayerStatsHandler playerStats;
+
+    [SerializeField]
+    private BoostStatHandler boostStats;
+
+    [SerializeField]
+    private int baseDamage = 5;
+
+    public int CalculateDamage() {
+        var playerDamageLevel = playerStats.GetOrCreateStat(PlayerStatType.Damage);
+        var damageBoostLevel = boostStats.GetOrCreateStat(BoostType.Damage);
+
+        int attackDamage = baseDamage + Mathf.RoundToInt(3.5f * playerDamageLevel.Value) + Mathf.RoundToInt(2 * damageBoostLevel.Value);
+
+        return attackDamage;
+
+    }
+
+}
