01bbd88ca0f96485dcff356815a91a2efe5274d4
CoenSteuns <c.steuns@gmail.com>
2019-05-23 11:15:43 +0200

    Do Da Wave

--- Assets/Scripts/PickUp/PickupCheerAnimationHandler.cs
+++ Assets/Scripts/PickUp/PickupCheerAnimationHandler.cs
@@ -3,40 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using PeppaSquad.Utils;
 
-public class PickupCheerAnimationHandler : MonoBehaviour {
+namespace PeppaSquad.Pickups.Animations {
+    public class PickupCheerAnimationHandler : MonoBehaviour {
 
-    [SerializeField]
-    private Animator animator;
+        [SerializeField]
+        private Animator animator;
 
-    [SerializeField]
-    private float cheerTime = 4f;
+        [SerializeField]
+        private float cheerTime = 4f;
 
-    [SerializeField]
-    private NumberRange cheerCooldownRange;
+        [SerializeField]
+        private NumberRange cheerCooldownRange;
 
-    private const string cheerAnimationKey = "IsCheering";
+        private const string cheerAnimationKey = "IsCheering";
 
-    private Coroutine randomCheerRoutine;
+        private Coroutine randomCheerRoutine;
 
-    private void Awake() {
-        randomCheerRoutine = StartCoroutine(RandomCheering());
-    }
-
-    public void Cheer() {
-        animator.SetBool(cheerAnimationKey, true);
-        StartCoroutine(CheerCoroutine());
-    }
-
-    private IEnumerator RandomCheering() {
-        while (true) {
-            yield return new WaitForSeconds(cheerCooldownRange.GetRandom());
-            Cheer();
+        private void Awake() {
+            randomCheerRoutine = StartCoroutine(RandomCheering());
         }
-    }
 
-    private IEnumerator CheerCoroutine() {
-        yield return new WaitForSeconds(cheerTime);
-        animator.SetBool(cheerAnimationKey, false);
-    }
+        public void Cheer() {
+            animator.SetBool(cheerAnimationKey, true);
+            StartCoroutine(CheerCoroutine());
+        }
 
+        private IEnumerator RandomCheering() {
+            while (true) {
+                yield return new WaitForSeconds(cheerCooldownRange.GetRandom());
+                Cheer();
+            }
+        }
+
+        private IEnumerator CheerCoroutine() {
+            yield return new WaitForSeconds(cheerTime);
+            animator.SetBool(cheerAnimationKey, false);
+        }
+
+    }
 }
