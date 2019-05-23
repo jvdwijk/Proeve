ea50406e98a346f735a38a67b327431125e0f00c
CoenSteuns <c.steuns@gmail.com>
2019-05-23 13:33:19 +0200

    added AttackDamageCalculator to combat namespace

--- Assets/Scripts/Combat/AttackDamageCalculator.cs
+++ Assets/Scripts/Combat/AttackDamageCalculator.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 using PeppaSquad.Pickups.Stats;
 using PeppaSquad.Stats.PlayerStats;
 
-public class AttackDamageCalculator : MonoBehaviour {
+namespace PeppaSquad.Combat {
+    public class AttackDamageCalculator : MonoBehaviour {
 
-    [SerializeField]
-    private PlayerStatsHandler playerStats;
+        [SerializeField]
+        private PlayerStatsHandler playerStats;
 
-    [SerializeField]
-    private BoostStatHandler boostStats;
+        [SerializeField]
+        private BoostStatHandler boostStats;
 
-    [SerializeField]
-    private int baseDamage = 5;
+        [SerializeField]
+        private int baseDamage = 5;
 
-    public int CalculateDamage() {
-        var playerDamageLevel = playerStats.GetOrCreateStat(PlayerStatType.Damage);
-        var damageBoostLevel = boostStats.GetOrCreateStat(BoostType.Damage);
+        public int CalculateDamage() {
+            var playerDamageLevel = playerStats.GetOrCreateStat(PlayerStatType.Damage);
+            var damageBoostLevel = boostStats.GetOrCreateStat(BoostType.Damage);
 
-        //TODO Make and test better damage calculation
-        int attackDamage = baseDamage + Mathf.RoundToInt(3.5f * playerDamageLevel.Value) + Mathf.RoundToInt(2 * damageBoostLevel.Value);
+            //TODO Make and test better damage calculation
+            int attackDamage = baseDamage + Mathf.RoundToInt(3.5f * playerDamageLevel.Value) + Mathf.RoundToInt(2 * damageBoostLevel.Value);
 
-        return attackDamage;
+            return attackDamage;
+
+        }
 
     }
-
 }
