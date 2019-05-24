132d834ace2d3154970d9516816063e3994970f4
CoenSteuns <c.steuns@gmail.com>
2019-05-24 09:05:53 +0200

    Deleted test script

--- Assets/Scripts/Utils/GameObjectDestroyer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameObjectDestroyer : MonoBehaviour {
-    public void DestroyThisObject() {
-        Destroy(this.gameObject);
-    }
-}
