c2eba2920426849d377c29967c32c042559577b5
unknown <c.steuns@gmail.com>
2019-05-27 11:31:17 +0200

    Power attack button spawning

---
+++ Assets/Scripts/Combat/Combo Attacks/ComboCriticalPointSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboCriticalPointSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 offset;
+    [SerializeField]
+    private Vector3 size;
+    [SerializeField]
+    private Quaternion rotation;
+
+    [SerializeField]
+    private bool drawGizmos;
+
+    private void OnDrawGizmos() {
+
+        if (!drawGizmos)
+            return;
+
+        Gizmos.DrawWireCube(transform.position + offset, size);
+    }
+
+}
---
+++ Assets/Scripts/Spawning/TransformCalculators/Rotation/LookAtCameraRotationCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PeppaSquad.Spawning.TransformCalculators.Base;
+
+namespace PeppaSquad.Spawning.TransformCalculators.Rotation {
+    public class LookAtCameraRotationCalculator : RotationCalculator {
+
+        private new Camera camera;
+
+        [SerializeField]
+        public Vector3 offset;
+
+        public override Quaternion CalculateRotation(Transform obj) {
+            Camera usedCamera = camera != null ? camera : Camera.main;
+            Vector3 relativePos = usedCamera.transform.position - obj.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+            rotation = Quaternion.Euler(rotation.eulerAngles + offset);
+            return rotation;
+        }
+    }
+}
