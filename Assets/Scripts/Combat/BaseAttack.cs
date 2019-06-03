82db6bef627e041b223a9ecfd68f98fb960cb983
jvdwijk <joeyvdwijk@gmail.com>
2019-06-03 17:02:24 +0200

    BaseAttack Commented

--- Assets/Scripts/Combat/BaseAttack.cs
+++ Assets/Scripts/Combat/BaseAttack.cs
@@ -8,6 +8,10 @@ namespace PeppaSquad.Combat {
         protected int attackDamage = 1;
         public event Action OnAttack;
 
+        /// <summary>
+        /// Damages the target
+        /// </summary>
+        /// <param name="target">The entity you want</param>
         public virtual void Attack(IDamagable target) {
             if (target == null) return;
             target.Damage(CalculateAttackDamage());
@@ -18,6 +22,10 @@ namespace PeppaSquad.Combat {
             return attackDamage;
         }
 
+        /// <summary>
+        /// Sets the attack damage :)
+        /// </summary>
+        /// <param name="damage">the new attack damage</param>
         public void SetAttackDamage(int damage) {
             attackDamage = damage;
         }
