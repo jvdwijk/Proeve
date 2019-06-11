bc343e64dbd38ff2adf10fd954857dab677aa3d4
jvdwijk <joeyvdwijk@gmail.com>
2019-06-11 16:48:46 +0200

    PickUp Instantiating

--- Assets/Scripts/PickUp/PickUpSpawner.cs
+++ Assets/Scripts/PickUp/PickUpSpawner.cs
@@ -11,22 +11,43 @@ namespace PeppaSquad.Pickups {
         private NumberRange numberRandomizer;
 
         [SerializeField]
-        private GameObject redWWPrefab, yellowWWPrefab, blueWWPrefab;
-
-        [SerializeField]
         private Transform[] spawnPositions;
 
         [SerializeField]
-        private int redAmount, yellowAmount, blueAmount;
+        private GameObject[] wwPrefab;
+
+        [SerializeField]
+        private int[] colorAmount;
 
         public override void TriggerReset () {
             DestroyPickUps();
+            numberRandomizer.SetRange(0, spawnPositions.Length);
             base.TriggerReset();
         }
 
         public void SpawnPickups () {
+            for (int size = 0; size < colorAmount.Length; size++)
+            {
+                for (int pickUp = 0; pickUp < colorAmount[size]; pickUp++)
+                {
+                    SetPickUp(wwPrefab[size]);
+                }
+            }
+        }
 
-            throw new System.NotImplementedException ();
+        private void SetPickUp(GameObject prefab){
+            bool wwSpawned = false;
+            while(wwSpawned){
+                var newSpot = GetRandomEmptySpot();
+                if(newSpot == null) continue;
+                Instantiate(prefab, newSpot);
+            }
+        }
+
+        private Transform GetRandomEmptySpot(){
+            int newNumber = (int)numberRandomizer.GetRandom();
+            
+            return spawnPositions[newNumber].childCount == 0 ? spawnPositions[newNumber] : null;
         }
 
         public void DestroyPickUps(){
@@ -34,12 +55,6 @@ namespace PeppaSquad.Pickups {
             {
                 Destroy(position.GetChild(0).gameObject);
             }
-        }
-
-        private int GetRandomEmptySpot(){
-            int newNumber = (int)numberRandomizer.GetRandom();
-            if(spawnPositions[newNumber].childCount > 0) return -1;
-            return newNumber;
         }
     }
 }
